072440dbebe02ef2f38dd89565727a7ce31caee0
Mahesh Nadar <nadarmahesh456@gmail.com>
2025-06-28 21:48:52 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    # You can add more songs here with their respective YouTube links
+    "Faded": "https://i.ytimg.com/vi/60ItHLz5WEA/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBpQorxwVHx23_TkJGAcRjBqw0gTQ",
+    "Alone": "https://www.youtube.com/watch?v=M-P4QBt-FWw&list=RDM-P4QBt-FWw&start_radio=1&pp=ygURYWxhbiB3YWxrZXIgc29uZ3OgBwHSBwkJwwkBhyohjO8%3D",
+    "lily": "https://www.youtube.com/watch?v=ox4tmEV6-QU&list=RDox4tmEV6-QU&start_radio=1&pp=ygURYWxhbiB3YWxrZXIgc29uZ3OgBwE%3D",
+}
